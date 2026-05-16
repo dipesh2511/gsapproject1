80ad5848f4a90a84c4ffc76a0aa32e196851efc2
dipesh <dipeshpohanekar1997@gmail.com>
2026-05-17 04:23:09 +0530

    Add initial project files including HTML, CSS, JS, and images

---
+++ index.js
@@ -0,0 +1,35 @@
+let timeline = gsap.timeline();
+
+timeline.from(".nav-text", {
+    opacity: 0.1,
+    y: -100,
+    duration: 1.5,
+    stagger: 0.3,
+    delay: 0.4
+})
+
+timeline.from(".hero-text", {
+    opacity: 0.1,
+    left: '-1000px',
+    duration: 1.5,
+    stagger: 0.5,
+    fontSize: '5vw',
+    delay: 0.4
+})
+
+timeline.from(".img", {
+    left: '80%',
+    rotate: 45,
+    stagger: 0.5,
+    opacity: 0,
+    duration: 1,
+    delay: 0.4,
+    scale: 1.5
+})
+
+timeline.from(".footer-text", {
+    opacity: 0.1,
+    y: -100,
+    duration: 1.5,
+    scale: 0.5,
+});
